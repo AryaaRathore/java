8d8d144b4fc85cf5599864a4596f46e6847404c5
AryaaRathore <aryarathore835@gmail.com>
2025-04-08 12:56:21 +0530

    msg

---
+++ demo.java
@@ -0,0 +1,18 @@
+public  class demo{
+    public static void main(String[] args){
+       // System.out.println("Hello world");
+       byte a =127;
+       int b = 45;
+       short c = 12;
+       double d = 23.6;
+       float e = 34.5f;
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(d);
+       System.out.println(e);
+       
+    
+
+    }
+}
