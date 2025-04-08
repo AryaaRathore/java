a0f86f1abb532ccf6a9efc988241a606ef84d624
AryaaRathore <aryarathore835@gmail.com>
2025-04-08 13:39:19 +0530

    hello

---
+++ day2.java
@@ -0,0 +1,11 @@
+public class day2 {
+    public static void main(String[] args){
+    int a=12,b=13;//swap without using third variable
+    a=a-b;
+    b=a+b;
+    a=b-a;
+    System.out.println(a);
+    System.out.println(b);
+
+}
+}
