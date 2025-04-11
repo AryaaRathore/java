6486f166ccf9b495483d56b1f4c006a511fa3b76
AryaaRathore <aryarathore835@gmail.com>
2025-04-11 10:11:54 +0530

    year

---
+++ grade.java
@@ -0,0 +1,24 @@
+public class grade {
+    public static void main(String[] args) {
+        int m = 90;
+        if(m>=90){
+            System.out.println("grade A");
+        }
+        else if (m>=70) {
+            System.out.println("grade B");
+            
+        }
+        else if (m>=65) {
+            System.out.println("C");
+        }
+        else if (m>=35) {
+            System.out.println("pass");
+            
+        }
+        else{
+            System.out.println("fail");
+        }
+
+    }
+    
+}
