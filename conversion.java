995f0aee827bc9a7e04987cce5e95eeccd2805fe
AryaaRathore <aryarathore835@gmail.com>
2025-04-24 22:26:58 +0530

    j

--- conversion.java
+++ conversion.java
@@ -1,7 +1,10 @@
 public class conversion {
     public static void main(String[] args) {
-        float a =12.5f;
-        int b =(int)a;
+        //float a =12.5f;
+        //int b =(int)a;
+        int a=12;
+        float b=(int)a;
+        System.out.println(a);
         System.out.println(b);
 
     }
