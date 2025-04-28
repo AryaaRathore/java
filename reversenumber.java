1e675e256fae056664dccab7c4a9896d84d25785
AryaaRathore <aryarathore835@gmail.com>
2025-04-28 15:57:47 +0530

    rev num

---
+++ reversenumber.java
@@ -0,0 +1,14 @@
+public class reversenumber {
+    static  void natuaralReverse(int n){
+        for(int i=n;i>=0;i--){
+            System.out.println(i);
+        }
+    
+
+    }
+    public static void main(String[] args) {
+        int n=100;
+        natuaralReverse(n);
+    }
+    
+}
