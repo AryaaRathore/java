40581b7273fb26fcf1ecdf844824e0fa5291977e
AryaaRathore <aryarathore835@gmail.com>
2025-04-28 21:01:42 +0530

    sum

--- sumNatural.java
+++ sumNatural.java
@@ -1,15 +1,13 @@
  class sumNatural {
     static void sum(int n){
         int sum=0;
-        for(int i=1;i<=n;i++){
-            
+        for(int i=1;i<=n;i++){ 
             sum=sum+i;
             System.out.println(sum);
         }
     } 
     public static void main(String[] args) {
-        int n=10;
-    
+        int n=100;
         sum(n);
     }
     
